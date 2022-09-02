f5fb54b9556a003d85a1bfcfde6c759c2adfbbb8
V.Bruk <vbruk@hey.com>
2022-09-02 15:02:04 -0300

    wip

--- test/models/sentence_test.rb
+++ test/models/sentence_test.rb
@@ -3,7 +3,14 @@
 require "test_helper"
 
 class SentenceTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
+  # test "valid sentence" do
+  #   sentence = Sentence.new(content: "Adidas wants to sell Puma for $1 Dollar")
+  #   assert sentence.valid?
+  # end
+
+  # test "not valid sentence without content" do
+  #   sentence = Sentence.new(content: "")
+  #   assert_not sentence.valid?
+  #   assert_not_nil sentence.errors[:content]
   # end
 end
