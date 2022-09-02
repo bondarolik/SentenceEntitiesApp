a863b058399d725edd9500d20e932b7a4cfaa082
V.Bruk <vbruk@hey.com>
2022-09-02 13:03:15 -0300

    add styles for entities

--- app/helpers/entities_helper.rb
+++ app/helpers/entities_helper.rb
@@ -1,4 +1,9 @@
 # frozen_string_literal: true
 
 module EntitiesHelper
+  def entity_formatted(entity)
+    tag.div class: entity.etype.downcase.to_s do
+      "#{entity.body} <span class='etype'>#{entity.etype}</span>".html_safe
+    end
+  end
 end
