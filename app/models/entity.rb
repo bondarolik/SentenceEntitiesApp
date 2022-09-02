6a6a1b7173bd92b3d2022325e0ef64f7b0af5ec9
V.Bruk <vbruk@hey.com>
2022-09-02 10:07:48 -0300

    add entity body validation

--- app/models/entity.rb
+++ app/models/entity.rb
@@ -1,7 +1,9 @@
 # frozen_string_literal: true
 
 class Entity < ApplicationRecord
+  enum etype: { org: 0, gpe: 1, money: 2, theme: 3, time: 4 }
+
   belongs_to :sentence
 
-  enum etype: { org: 0, gpe: 1, money: 2, theme: 3, time: 4 }
+  validates :body, presence: true
 end
