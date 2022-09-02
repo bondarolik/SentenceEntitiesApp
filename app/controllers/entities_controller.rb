86715b7ff0aa4e2754e4fb57eaac04f2a3dccb80
V.Bruk <vbruk@hey.com>
2022-09-02 11:10:29 -0300

    configure EntitiesController to use with Sentence

--- app/controllers/entities_controller.rb
+++ app/controllers/entities_controller.rb
@@ -2,8 +2,18 @@
 
 class EntitiesController < ApplicationController
   include DryController
+  before_action :set_sentence
+
+  def new
+    super
+    @resource = @sentence.entities.new
+  end
 
   private
+
+  def set_sentence
+    @sentence = Sentence.find(params[:sentence_id])
+  end
 
   def resource_params
     accessible = [:body, :etype, :sentence_id]
